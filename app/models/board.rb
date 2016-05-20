7993d87f36cae0686e833346b8f455637c5c2f50
meghankinney <meghankinney@users.noreply.github.com>
2016-05-20 20:50:26 +0000

    cards

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,6 +1,6 @@
 class Board < ActiveRecord::Base
 
-  #validates :name, :presence => true, :uniqueness => {:scope => :user_id}
+  validates :name, :presence => true, :uniqueness => {:scope => :user_id}
 
   has_many :lists, :class_name => "List", :foreign_key => "board_id"
   belongs_to :board
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,7 @@
 class List < ActiveRecord::Base
 
-  #validates :name, :presence => true, uniqueness => {:scope => :board_id}
-  #validates :board_id, :presence => true
+  validates :name, :presence => true, :uniqueness => {:scope => :board_id}
+  validates :board_id, :presence => true
 
   belongs_to :board
   has_many :cards
