b11d794c59cff40ee6fd928880c7b09a5dd7e4c2
alathans <alathans@users.noreply.github.com>
2016-05-20 15:19:31 -0500

    3:20 pm

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,6 @@
 class Card < ActiveRecord::Base
   validates :name, :presence => true
   validates :list_id, :presence => true
+
+  belongs_to :list
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,10 @@
 class List < ActiveRecord::Base
-  validates :name, :presence => true
+  validates :name, :presence => true,
             :uniqueness => {:scope => :user_id}
-  validates :board_id, :presence => true,
+  validates :board_id, :presence => true
+
+  belongs_to :board_id
+  has_many :cards
+  # here we're telling it, a list belongs to a specific board - should be able to say card.list,  list.card.... want to add helper methods for all the associations that are drawn in the diagram - a list belongs to one board, so we want the method that we called to also be singular... rails wants the code to read like english, so it needs to be board_id b/c it's one specific board
 
 end
