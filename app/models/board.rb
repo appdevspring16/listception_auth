b803f2ae9c993cf6db108a715714a29d16d27d48
alathans <alathans@users.noreply.github.com>
2016-05-20 14:52:56 -0500

    updates

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,2 +1,6 @@
 class Board < ActiveRecord::Base
+  validates :name, :presence => true,
+            :uniqueness => {:scope => :user_id}
+
+            # This says that diff users can have the same name, but within the set for that user, it needs to be unique...
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,6 @@
 class List < ActiveRecord::Base
+  validates :name, :presence => true
+            :uniqueness => {:scope => :user_id}
+  validates :board_id, :presence => true,
+
 end
