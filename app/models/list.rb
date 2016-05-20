63f453d7a253aaa1a1860361b90c7125367b4bed
stephenandrewberger <stephen.berger@chicagobooth.edu>
2016-05-20 15:53:01 -0500

    stopping_point_3

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,8 @@
 class List < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => {:scope => :board_id }
   validates :board_id, :presence => true
+
+  # belongs_to :board, :class_name => "Board", :foreign_key => "board_id"
+  belongs_to :board
+  has_many :cards
 end
